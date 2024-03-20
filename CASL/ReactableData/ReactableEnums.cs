a1a231cfc34e7e98e698079ae00ace5fb08cd594
Calvin Wilkinson <kinsondigital@gmail.com>
2024-03-20 08:16:00 +0000

    chore: create enums for audio commands using carbonate

---
+++ CASL/ReactableData/ReactableEnums.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="ReactableEnums.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace CASL.ReactableData;
+
+using Data;
+
+/// <summary>
+/// Represents the different types of audio commands to send in
+/// the <see cref="FullBuffer"/> and <see cref="StreamBuffer"/> types.
+/// </summary>
+internal enum AudioCommands
+{
+    /// <summary>
+    /// The play command.
+    /// </summary>
+    Play,
+
+    /// <summary>
+    /// The pause command.
+    /// </summary>
+    Pause,
+
+    /// <summary>
+    /// The reset command.
+    /// </summary>
+    Reset,
+
+    /// <summary>
+    /// Enable looping command.
+    /// </summary>
+    EnableLooping,
+
+    /// <summary>
+    /// Disable looping command.
+    /// </summary>
+    DisableLooping,
+}
