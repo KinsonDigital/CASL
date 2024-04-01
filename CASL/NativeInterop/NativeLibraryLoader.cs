7a2f443f351d938b427996a8db8fae2b211659d2
Calvin Wilkinson <kinsondigital@gmail.com>
2024-04-01 11:53:43 +0100

    refactor: improve exception message usage

--- CASL/NativeInterop/NativeLibraryLoader.cs
+++ CASL/NativeInterop/NativeLibraryLoader.cs
@@ -47,11 +47,12 @@ internal sealed class NativeLibraryLoader : ILibraryLoader
         IPath path,
         ILibrary library)
     {
-        this.dependencyManager = dependencyManager ?? throw new ArgumentNullException(nameof(dependencyManager), "The parameter must not be null.");
-        this.platform = platform ?? throw new ArgumentNullException(nameof(platform), "The parameter must not be null.");
-        this.directory = directory ?? throw new ArgumentNullException(nameof(directory), "The parameter must not be null.");
-        this.file = file ?? throw new ArgumentNullException(nameof(file), "The parameter must not be null.");
-        this.path = path ?? throw new ArgumentNullException(nameof(path), "The parameter must not be null.");
+        const string errorMsg = "The parameter must not be null.";
+        this.dependencyManager = dependencyManager ?? throw new ArgumentNullException(nameof(dependencyManager), errorMsg);
+        this.platform = platform ?? throw new ArgumentNullException(nameof(platform), errorMsg);
+        this.directory = directory ?? throw new ArgumentNullException(nameof(directory), errorMsg);
+        this.file = file ?? throw new ArgumentNullException(nameof(file), errorMsg);
+        this.path = path ?? throw new ArgumentNullException(nameof(path), errorMsg);
 
         if (library is null)
         {
