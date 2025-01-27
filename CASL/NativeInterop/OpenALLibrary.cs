bd8579b9491f039f0e1fbc467fe62767c701e4e9
CalvinWilkinson <kinsondigital@gmail.com>
2025-01-27 17:41:55 +0000

    bug: change how the openal native library is loaded

--- CASL/NativeInterop/OpenALLibrary.cs
+++ CASL/NativeInterop/OpenALLibrary.cs
@@ -4,6 +4,8 @@
 
 namespace CASL.NativeInterop;
 
+using System.IO;
+using System.IO.Abstractions;
 using Exceptions;
 
 /// <summary>
@@ -14,12 +16,32 @@ internal sealed class OpenALLibrary : ILibrary
     private const string WinLibName = "soft_oal.dll";
     private const string PosixLibName = "libopenal.so";
     private readonly IPlatform platform;
+    private readonly IDirectory directory;
+    private readonly IFile file;
+    private readonly IPath path;
+    private readonly string appDirPath;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OpenALLibrary"/> class.
     /// </summary>
     /// <param name="platform">Provides platform specific information.</param>
-    public OpenALLibrary(IPlatform platform) => this.platform = platform;
+    /// <param name="application">Provides application related services.</param>
+    public OpenALLibrary(
+        IPlatform platform,
+        IDirectory directory,
+        IFile file,
+        IPath path,
+        IApplication application)
+    {
+        // TODO: Add null checks for all params
+        this.platform = platform;
+        this.directory = directory;
+        this.file = file;
+        this.path = path;
+        this.appDirPath = application.Location;
+
+        ProcessLibFile();
+    }
 
     /// <inheritdoc/>
     public string GetLibraryName()
@@ -34,6 +56,54 @@ internal sealed class OpenALLibrary : ILibrary
             return PosixLibName;
         }
 
-        throw new UnknownPlatformException($"The platform '{this.platform.CurrentOSPlatform}' is unknown.");
+        throw new UnknownPlatformException($"The platform '{this.platform.CurrentOSPlatform}' is unknown or not supported.");
+    }
+
+    /// <summary>
+    /// Checks if the OpenAL library located in the same location as CASL.
+    /// If not, it will check if the correct runtime/platform directory exists, and if so
+    /// copies the library to the same location as CASL.
+    /// </summary>
+    private void ProcessLibFile()
+    {
+        var libName = GetLibraryName();
+        var fullLibPath = $"{this.appDirPath}{this.path.DirectorySeparatorChar}{libName}";
+
+        // Check if the library exists in the same location as the application
+        if (this.file.Exists(fullLibPath))
+        {
+            return;
+        }
+
+        var platformDirName = string.Empty;
+
+        if (this.platform.IsWinPlatform())
+        {
+            platformDirName = "win";
+        }
+        else if (this.platform.IsPosixPlatform())
+        {
+            platformDirName = "linux";
+        }
+
+        var platformDirPath = $"{this.appDirPath}{this.path.DirectorySeparatorChar}runtimes{this.path.DirectorySeparatorChar}" +
+                              $"{platformDirName}-x64{this.path.DirectorySeparatorChar}native";
+
+        // If the correct runtime/platform directory does not exist
+        if (!this.directory.Exists(platformDirPath))
+        {
+            throw new DirectoryNotFoundException($"The directory '{platformDirPath}' does not exist.");
+        }
+
+        var fullPlatLibPath = $"{platformDirPath}{this.path.DirectorySeparatorChar}{libName}";
+
+        // If the library does not exist in the platform directory
+        if (!this.file.Exists(fullPlatLibPath))
+        {
+            throw new FileNotFoundException($"The library '{libName}' does not exist in the platform directory '{platformDirPath}'.");
+        }
+
+        // At this point, the library file exist.  Copy the library to the same location as the application.
+        this.file.Copy(fullPlatLibPath, fullLibPath);
     }
 }
--- Testing/CASLTests/NativeInterop/OpenALLibraryTests.cs
+++ Testing/CASLTests/NativeInterop/OpenALLibraryTests.cs
@@ -6,6 +6,9 @@
 namespace CASLTests.NativeInterop;
 
 #pragma warning disable IDE0001 // The name can be simplified
+using System.IO;
+using System.IO.Abstractions;
+using CASL;
 using CASL.Exceptions;
 using CASL.NativeInterop;
 using Xunit;
@@ -20,59 +23,156 @@ using NSubstitute;
 public class OpenALLibraryTests
 {
     private readonly IPlatform mockPlatform;
+    private readonly IDirectory mockDirectory;
+    private readonly IFile mockFile;
+    private readonly IPath mockPath;
+    private readonly IApplication mockApplication;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OpenALLibraryTests"/> class.
     /// </summary>
-    public OpenALLibraryTests() => this.mockPlatform = Substitute.For<IPlatform>();
+    public OpenALLibraryTests()
+    {
+        this.mockPlatform = Substitute.For<IPlatform>();
+        this.mockDirectory = Substitute.For<IDirectory>();
+        this.mockFile = Substitute.For<IFile>();
+        this.mockPath = Substitute.For<IPath>();
+        this.mockApplication = Substitute.For<IApplication>();
+    }
 
-    #region Prop Tests
+
+    // TODO: Add ctor tests for null params
+
+    #region Constructor Tests
     [Fact]
-    public void LibraryName_WhenGettingValueWithWindowsPlatform_ReturnsCorrectResult()
+    public void Ctor_WithUnknownPlatform_ThrowsException()
+    {
+        // Arrange
+        this.mockPlatform.IsWinPlatform().Returns(false);
+        this.mockPlatform.IsPosixPlatform().Returns(false);
+        this.mockPlatform.CurrentOSPlatform.Returns("xyz");
+
+        // Act
+        var act = CreateSystemUnderTest;
+
+        // Assert
+        act.Should().Throw<UnknownPlatformException>()
+            .WithMessage("The platform 'xyz' is unknown or not supported.");
+    }
+
+    [Fact]
+    public void Ctor_WhenPlatformDirPathDoesNotExist_ThrowsException()
     {
         // Arrange
         MockWindowsPlatform();
-        var library = new OpenALLibrary(this.mockPlatform);
+        this.mockApplication.Location.Returns(@"C:\app-dir");
+        this.mockFile.Exists(Arg.Any<string>()).Returns(false);
+        this.mockDirectory.Exists(Arg.Any<string>()).Returns(false);
 
         // Act
-        var actual = library.GetLibraryName();
+        var act = CreateSystemUnderTest;
+
+        // Assert
+        act.Should().Throw<DirectoryNotFoundException>()
+            .WithMessage(@"The directory 'C:\app-dir\runtimes\win-x64\native' does not exist.");
+    }
+
+    [Fact]
+    public void Ctor_WhenFullPlatformLibFilePathDoesNotExist_ThrowsException()
+    {
+        // Arrange
+        const string expected = @"The library 'soft_oal.dll' does not exist" +
+                                @" in the platform directory 'C:\app-dir\runtimes\win-x64\native'.";
+        MockWindowsPlatform();
+        this.mockApplication.Location.Returns(@"C:\app-dir");
+        this.mockFile.Exists(Arg.Any<string>()).Returns(false);
+        this.mockDirectory.Exists(Arg.Any<string>()).Returns(true);
+
+        // Act
+        var act = CreateSystemUnderTest;
+
+        // Assert
+        act.Should().Throw<FileNotFoundException>()
+            .WithMessage(expected);
+    }
+
+    [Fact]
+    public void Ctor_WithWindowsPlatform_ProcessesLibFile()
+    {
+        // Arrange
+        MockWindowsPlatform();
+        this.mockFile.Exists(@"C:\app-dir\soft_oal.dll").Returns(false);
+        this.mockDirectory.Exists(Arg.Any<string>()).Returns(true);
+        this.mockFile.Exists(@"C:\app-dir\runtimes\win-x64\native\soft_oal.dll").Returns(true);
+
+        // Act
+        CreateSystemUnderTest();
+
+        // Assert
+        this.mockFile.Received(1)
+            .Copy(@"C:\app-dir\runtimes\win-x64\native\soft_oal.dll", @"C:\app-dir\soft_oal.dll");
+    }
+
+    [Fact]
+    public void Ctor_WithPosixPlatforms_ProcessesLibFile()
+    {
+        // Arrange
+        MockPosixPlatform();
+        this.mockFile.Exists("/app-dir/libopenal.so").Returns(false);
+        this.mockDirectory.Exists(Arg.Any<string>()).Returns(true);
+        this.mockFile.Exists("/app-dir/runtimes/linux-x64/native/libopenal.so").Returns(true);
+
+        // Act
+        CreateSystemUnderTest();
+
+        // Assert
+        this.mockFile.Received(1)
+            .Copy("/app-dir/runtimes/linux-x64/native/libopenal.so", "/app-dir/libopenal.so");
+    }
+    #endregion
+
+    #region Method Tests
+    [Fact]
+    public void GetLibraryName_WhenGettingValueWithWinPlatform_ReturnsCorrectResult()
+    {
+        // Arrange
+        MockWindowsPlatform();
+        this.mockFile.Exists(Arg.Any<string>()).Returns(true);
+        var sut = CreateSystemUnderTest();
+
+        // Act
+        var actual = sut.GetLibraryName();
 
         // Assert
         actual.Should().Be("soft_oal.dll");
     }
 
     [Fact]
-    public void LibraryName_WhenGettingValueWithPosixPlatform_ReturnsCorrectResult()
+    public void GetLibraryName_WhenGettingValueWithPosixPlatform_ReturnsCorrectResult()
     {
         // Arrange
         MockPosixPlatform();
-        var library = new OpenALLibrary(this.mockPlatform);
+        this.mockFile.Exists(Arg.Any<string>()).Returns(true);
+        var sut = CreateSystemUnderTest();
 
         // Act
-        var actual = library.GetLibraryName();
+        var actual = sut.GetLibraryName();
 
         // Assert
         actual.Should().Be("libopenal.so");
     }
-
-    [Fact]
-    public void LibraryName_WithUnknownPlatform_ThrowsException()
-    {
-        // Arrange
-        this.mockPlatform.IsWinPlatform().Returns(false);
-        this.mockPlatform.IsPosixPlatform().Returns(false);
-        this.mockPlatform.CurrentOSPlatform.Returns("unknown-platform");
-
-        var library = new OpenALLibrary(this.mockPlatform);
-
-        // Act
-        var act = () => library.GetLibraryName();
-
-        // Assert
-        act.Should().Throw<UnknownPlatformException>()
-            .WithMessage("The platform 'unknown-platform' is unknown.");
-    }
     #endregion
+
+    /// <summary>
+    /// Creates a new instance of <see cref="OpenALLibrary"/> for the purpose of testing.
+    /// </summary>
+    /// <returns>The instance to test.</returns>
+    private OpenALLibrary CreateSystemUnderTest()
+        => new (this.mockPlatform,
+            this.mockDirectory,
+            this.mockFile,
+            this.mockPath,
+            this.mockApplication);
 
     /// <summary>
     /// Mocks a windows platform.
@@ -81,6 +181,8 @@ public class OpenALLibraryTests
     {
         this.mockPlatform.IsWinPlatform().Returns(true);
         this.mockPlatform.IsPosixPlatform().Returns(false);
+        this.mockPath.DirectorySeparatorChar.Returns('\\');
+        this.mockApplication.Location.Returns(@"C:\app-dir");
     }
 
     /// <summary>
@@ -90,5 +192,7 @@ public class OpenALLibraryTests
     {
         this.mockPlatform.IsWinPlatform().Returns(false);
         this.mockPlatform.IsPosixPlatform().Returns(true);
+        this.mockPath.DirectorySeparatorChar.Returns('/');
+        this.mockApplication.Location.Returns("/app-dir");
     }
 }
