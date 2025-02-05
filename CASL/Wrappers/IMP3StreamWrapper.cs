bac8a3092ae6f1392c4a68149912d97b59ce4e1d
CalvinWilkinson <kinsondigital@gmail.com>
2025-02-05 16:47:20 +0000

    chore: create thin wrapper for the mp3 stream api

---
+++ CASL/Wrappers/IMP3StreamWrapper.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="IMP3StreamWrapper.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace CASL.Wrappers;
+
+using System;
+using MP3Sharp;
+
+/// <summary>
+/// A thin wrapper around the mp3 stream API for the purpose of abstraction and testing.
+/// </summary>
+internal interface IMP3StreamWrapper : IDisposable
+{
+    /// <inheritdoc cref="MP3Stream.ChannelCount"/>
+    int ChannelCount { get; }
+
+    /// <inheritdoc cref="MP3Stream.Format"/>
+    SoundFormat Format { get; }
+
+    /// <inheritdoc cref="MP3Stream.Frequency"/>
+    int Frequency { get; }
+
+    /// <summary>
+    /// Loads a mp3 file at the given <paramref name="filePath"/>.
+    /// </summary>
+    /// <param name="filePath">The path to the file.</param>
+    void Load(string filePath);
+
+    /// <inheritdoc cref="MP3Stream.Read(byte[],int,int)"/>
+    int Read(byte[] buffer, int offset, int count);
+
+    /// <summary>
+    /// <inheritdoc cref="MP3Stream.Flush"/>
+    /// </summary>
+    void Flush();
+}
