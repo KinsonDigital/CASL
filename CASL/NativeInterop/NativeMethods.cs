105a617e2574096a2f7f8e234c25e751a63c368e
CalvinWilkinson <kinsondigital@gmail.com>
2026-07-03 08:40:30 +0100

    refactor: improve code and exclude from code coverage

--- CASL/NativeInterop/NativeMethods.cs
+++ CASL/NativeInterop/NativeMethods.cs
@@ -5,11 +5,13 @@
 namespace CASL.NativeInterop;
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 /// <summary>
 /// Provides access to interop with native windows and posix platform related functions.
 /// </summary>
+[ExcludeFromCodeCoverage(Justification = "Interop with native methods is not covered by unit tests.")]
 internal static partial class NativeMethods
 {
     private const string WinLibName = "kernel32.dll";
@@ -19,23 +21,18 @@ internal static partial class NativeMethods
     /// <summary>
     /// Initializes static members of the <see cref="NativeMethods"/> class.
     /// </summary>
-    static NativeMethods()
-    {
+    static NativeMethods() =>
         NativeLibrary.SetDllImportResolver(
             typeof(NativeMethods).Assembly,
             (libraryName, assembly, searchPath) =>
             {
-                if (libraryName == PosixLibName && RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                if (libraryName != PosixLibName || !RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
-                    if (NativeLibrary.TryLoad(MacLibName, assembly, searchPath, out var handle))
-                    {
-                        return handle;
-                    }
+                    return IntPtr.Zero;
                 }
 
-                return IntPtr.Zero;
+                return NativeLibrary.TryLoad(MacLibName, assembly, searchPath, out var handle) ? handle : IntPtr.Zero;
             });
-    }
 
     /// <summary>
     /// Loads the specified module into the address space of the calling process. The specified module may cause other modules to be loaded.
